975427afbb6d419c73782e32e191595e38cd2ff9
CORP\ambansal <amit.bansal@capgemini.com>
2018-09-18 11:42:34 +0530

    Updated

--- src/main/java/com/capgemini/login/LoginService.java
+++ src/main/java/com/capgemini/login/LoginService.java
@@ -3,7 +3,7 @@ package com.capgemini.login;
 public class LoginService {
 
 	public boolean isUserValid(String user, String password) {
-		if (user.equals("rahul") && password.equals("springone"))
+		if (user.equals("murtaza") && password.equals("springone"))
 			return true;
 
 		return false;
